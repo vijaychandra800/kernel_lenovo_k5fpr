49695b981b55bd5907b597ec0927d2f86c94f56a
Sahil Sonar <sss.sonar2003@outlook.com>
2019-02-13 03:32:03 +0000

    script : make use of all thread and create dir tools/tools

--- mohan.sh
+++ mohan.sh
@@ -1,9 +1,10 @@
-export KBUILD_BUILD_USER="Mohancm"
-export KBUILD_BUILD_HOST="TEAM_ZERO"
-export CROSS_COMPILE="/home/mohanmanjappa/aarch64-linux-android-4.9/bin/aarch64-linux-android-"
+export KBUILD_BUILD_USER="SahilSonar"
+export KBUILD_BUILD_HOST="BETA_BUILDS"
+export CROSS_COMPILE="/home/cromproj/aarch64-linux-android-4.9/bin/aarch64-linux-android-"
+export ARCH=arm64 && export SUBARCH=arm64
 
-make clean && make mrproper
-
-mkdir out
-
-make ARCH=arm64 O=out k5fpr_defconfig;make ARCH=arm64 -j30 O=out ;
+make clean
+make mrproper
+mkdir -p tools/tools
+make k5fpr_defconfig
+make -j$(nproc --all)
